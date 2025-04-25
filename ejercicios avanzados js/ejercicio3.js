34299acde5d20462b76869ac7a39fd6362279c4e
Juanita Zuluaga Acosta <59172512+cantsbee@users.noreply.github.com>
2025-04-25 17:00:39 +0200

    Update ejercicio3.js

---
+++ ejercicios avanzados js/ejercicio3.js
@@ -0,0 +1,53 @@
+const users = [
+  {
+    name: "Alberto",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Antonio",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Santiago",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Laura",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+// Objeto para llevar el conteo de los sonidos favoritos
+const soundCount = {};
+
+
+for (const user of users) {
+  // Recorremos los sonidos favoritos de cada usuario
+  for (const sound in user.favoritesSounds) {
+    
+    if (soundCount[sound]) {
+      soundCount[sound]++;
+    } else {
+     
+      soundCount[sound] = 1;
+    }
+  }
+}
+
+console.log(soundCount);
